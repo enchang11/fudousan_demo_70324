f9f8ac0020e2c1ba71bb4f78c6540292ce278166
Rachelle Valera <enchang-designlab@it-tera.com>
2015-03-25 14:00:37 +0800

    1

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+require('socket.io').version
+var server = app.listen(3003);
+var io = require('socket.io').listen(server);
+
+app.use(express.static(__dirname + '/public'));
+
+
+/*var express    = require('express');
+var app        = express.createServer();
+var io         = require('socket.io').listen(app);
+
+app.configure(function(){
+	app.use(express.static(__dirname + "/public"));
+});
+
+app.listen(3281);*/
+
+//remove error log
+io.set('log level', 1);
